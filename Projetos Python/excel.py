f4041f3bbc7ee7c648e2991a392d6b6e98b67a22
TatiiNogueira <68739197+TatiiNogueira@users.noreply.github.com>
2020-08-13 11:32:55 +0100

    Add files via upload

---
+++ Projetos Python/excel.py
@@ -0,0 +1,15 @@
+#Ler um ficheiro excel, e criar um "resumo"
+
+#Módulos
+import pandas as pd
+import sweetviz as sv
+import numpy as np 
+
+#Fciheiro excel
+x = pd.read_excel('C:\\Users\\nogue\\Desktop\\Work\\Python\\anuncios.xlsx')
+#Imprimir a tabela/informação do excel
+print(x) 
+
+#Irá abrir uma janela com as informações da minha tabela por coluna
+#Cria o reporte Exportar o reporte
+sv.analyze(x).show_html()

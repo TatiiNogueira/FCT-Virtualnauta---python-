b34fa0e29fa2fbe88ed4ab69092c33e0ae74dbf2
TatiiNogueira <68739197+TatiiNogueira@users.noreply.github.com>
2020-08-18 11:46:48 +0100

    Delete Teste.py

--- Tutorial Chorme Driver/Teste.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-#Passar o caminho do webdriver
-driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
-
-#Para ver se está a funcionar, utilizo um site qualquer
-driver.get('https://eco.sapo.pt')
-

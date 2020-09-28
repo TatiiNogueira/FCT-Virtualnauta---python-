f7dd8e891462a43c002a2313618beb09b6d55d70
TatiiNogueira <68739197+TatiiNogueira@users.noreply.github.com>
2020-09-28 13:59:04 +0100

    Add files via upload

---
+++ Projetos Python/Instalar_Bibliotecas.py
@@ -0,0 +1,14 @@
+
+#Instalação das bibliotecas do python de forma automática
+
+#Módulos
+from pip._internal import main as pipmain
+
+#Lista das bibliotecas
+list = ('pika','beautifulsoup4','bs4','flask','matplotlib','numpy','openpyxl','pyPDF2','selenium','pandas','django','gunicorn','pyautogui','pywin32','keyboard','opencv-python','pyautogui')
+
+for element in list:
+    #Instalar as bibliotecas do python
+    pipmain(['install', f"{element}"])
+    #print(element)
+print("INSTALAÇÃO CONLUÍDA, TODAS AS BIBLIOTECAS FORAM INSTALADAS")
